c57c645075f29cc500c0297995daf59819bbf329
loharvikas14 <vikas@cognisaas.com>
2022-11-17 08:25:57 +0530

    Initial commit

---
+++ app/src/main/java/ru/kotlin/a20012011057_Vikas_Lohar_Prac_6/YoutubeActivity.kt
@@ -0,0 +1,26 @@
+package ru.kotlin.a20012011057_Vikas_Lohar_Prac_6
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class YoutubeActivity : AppCompatActivity() {
+    private val youtubeId="MN8p-Vrn6G0"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+        val mywebview=findViewById<WebView>(R.id.web)
+        val settings=mywebview.settings
+        settings.javaScriptEnabled=true
+        settings.loadWithOverviewMode=true
+        settings.useWideViewPort=true
+
+        mywebview.loadUrl("https://www.youtube.com/embed/$youtubeId")
+        val button=findViewById<FloatingActionButton>(R.id.webfloat)
+        button.setOnClickListener{
+            Intent(this,MainActivity::class.java).apply{startActivity(this)}
+    }
+}
+}
